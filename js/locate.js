d0583bf9dc4e5148410e307bbaa18c1263cd9246
dimitri ntempos <ntemposd@gmail.com>
2015-10-10 22:30:55 +0300

    saturday night commit
    location works on test server
    see: needit.ntemposd.me

--- js/locate.js
+++ js/locate.js
@@ -1,20 +1,17 @@
 
-var x = document.getElementById("demo");
+	var x = document.getElementById("demo");
 
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
+    } else { 
         x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
 
 function showPosition(position) {
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
-
-    var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
-    +latlon+"&zoom=14&size=400x300&sensor=false";
-    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;	
 }
 
 function showError(error) {
